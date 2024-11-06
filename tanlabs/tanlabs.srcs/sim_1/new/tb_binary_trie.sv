c09e04fd0e3a7bb96366f13afa038ac825b18133
Jason Fu <fjs22@mails.tsinghua.edu.cn>
2024-11-06 17:53:36 +0800

    fix: tb_binary_trie wiring fixed

--- tanlabs/tanlabs.srcs/sim_1/new/tb_binary_trie.sv
+++ tanlabs/tanlabs.srcs/sim_1/new/tb_binary_trie.sv
@@ -7,6 +7,8 @@ module tb_binary_trie;
   logic clk;
   logic rst_p;
 
+  logic [127:0] pipeline_prefix;  // TODO: pipeline_prefix is file input
+
   // ===========================
   // Binary Trie
   // ===========================
@@ -23,9 +25,9 @@ module tb_binary_trie;
   logic [127:0] prefix_i[16];
   logic [7:0] prefix_length_i[16];  // input should be 8N
   logic [4:0] next_hop_addr_i[16];
-  fw_frame_beat_t frame_beat_i[16];
-  logic valid_i[16];
-  logic skip_i[16];
+  fw_frame_beat_t frame_beat_i[16];  // TODO: frame_beat_i[0]
+  logic valid_i[16];  // TODO: valid_i[0]
+  logic skip_i[16];  // TODO: skip_i[0]
   logic ready_o[16];  // in ready
 
   // pipeline outputs
@@ -46,6 +48,40 @@ module tb_binary_trie;
   logic [12:0] addrb[16];
   logic [35:0] dinb[16];
   logic [35:0] doutb[16];
+
+  // input
+  always_ff @(posedge clk) begin : PinelineInput
+    if (rst_p) begin
+      max_match_i[0] <= 0;
+      prefix_length_i[0] <= 0;
+      next_hop_addr_i[0] <= 0;
+      prefix_i[0] <= 0;
+    end else begin
+      if (ready_o[0]) begin
+        max_match_i[0] <= 0;
+        prefix_length_i[0] <= 0;
+        next_hop_addr_i[0] <= 0;
+        prefix_i[0] <= pipeline_prefix;
+      end
+    end
+  end
+
+
+  // link the pipeline
+  generate
+    for (i = 0; i < 15; i = i + 1) begin : g_links
+      assign max_match_i[i+1] = max_match_o[i];
+      assign prefix_i[i+1] = prefix_o[i];
+      assign prefix_length_i[i+1] = prefix_length_o[i];
+      assign next_hop_addr_i[i+1] = next_hop_addr_o[i];
+      assign frame_beat_i[i+1] = frame_beat_o[i];
+      assign valid_i[i+1] = valid_o[i];
+      assign skip_i[i+1] = skip_o[i];
+      assign ready_i[i] = ready_o[i+1];
+
+      assign init_addr_i[i+1] = next_addr_o[i];
+    end
+  endgenerate
 
   // Instantiate the Unit Under Test (UUT)
   generate
